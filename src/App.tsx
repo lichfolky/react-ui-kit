10e727a3a3eb1e7ea6ba3bf673e6b1e7787cd3a0
Lichfolky <lichfolky@gmail.com>
2023-10-05 13:18:55 +0200

    radio group

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import CountrySelect from './components/CountrySelect';
+import RadioGroup from './components/RadioGroup';
 import TwoFactor from './components/TwoFactors';
 
 function App() {
+  const shapes = [{ value: "circles", label: "circles" }, { value: "poli", label: "poli" }];
+
 
   return (
     <>
-      <TwoFactor />
+      <RadioGroup name="shapes" values={shapes} required />
     </>
   );
 }
---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function RadioGroup({ name, values, required }: { name: string; values: { value: string, label: string; }[]; required: boolean; }) {
+    const [
+        selectedValue,
+        setSelectedValue,
+    ] = React.useState('');
+
+    return (<>{selectedValue}
+        <div className="radio-wrapper">
+            {values.map(({ value, label }) => (
+                < div className="radio-option" key={value}>
+                    <input
+                        id="shape-polygon"
+                        type="radio"
+                        name={name}
+                        value={value}
+                        checked={selectedValue === value}
+                        onChange={event => {
+                            setSelectedValue(event.target.value);
+                        }}
+                        required={required}
+                    />
+                    <label htmlFor="shape-polygon">
+                        {label}
+                    </label>
+                </div>))
+            }
+        </div ></>);
+}
+
+
+export default RadioGroup;
